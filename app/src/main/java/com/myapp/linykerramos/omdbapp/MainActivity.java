db0d3d294ecde4d620ccd4bdc04e18ce21f061f2
Linyker <linykerrm@gmail.com>
2016-06-17 14:39:17 -0300

    correção do menu lateral

--- app/src/main/java/com/myapp/linykerramos/omdbapp/MainActivity.java
+++ app/src/main/java/com/myapp/linykerramos/omdbapp/MainActivity.java
@@ -95,13 +95,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.registered_movies) {
             RegisteredMovies registeredMovies = new RegisteredMovies();
             fragmentTransaction.replace(R.id.fragment_container, registeredMovies).commit();
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
